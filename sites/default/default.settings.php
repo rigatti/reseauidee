62396af4cedb32d08e7cd6501a34cad2b5fa3362
Michel <michel.rigatti@gmail.com>
2018-12-21 16:19:14 +0100

    update to 8.6.4

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -88,8 +88,16 @@
  * );
  * @endcode
  */
-$databases = [];
-
+$databases['default']['default'] = array (
+  'database' => 'reseauidee',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
 /**
  * Customizing database settings.
  *
